e2c35c24b3c15f8e06446087f046658787de4c9c
George Arbanas <george.arbanas@icloud.com>
2023-10-24 13:35:46 +0900

    added basic menu screen linking -- next will be to extract specific sections of the pngs to display when loaded

--- doom.js
+++ doom.js
@@ -1,4 +1,9 @@
 const gameWindow = document.querySelector("body");
-window.addEventListener("load", (e) => {
+const gameStart = gameWindow.querySelector(".page-loader");
+
+function startGame(e) {
+  console.log("game started!");
   console.log(e);
-});
+}
+
+gameStart.addEventListener("click", startGame);
