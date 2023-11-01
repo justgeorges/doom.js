4f170631e843e2d562287f7f5e3cddc2934f6b29
George Arbanas <george.arbanas@icloud.com>
2023-11-01 19:54:35 +0900

    added title screen transition

--- doom.js
+++ doom.js
@@ -1,10 +1,12 @@
 const gameWindow = document.querySelector("body");
+const titleScreen = gameWindow.querySelector(".title-screen");
 let gameStarted = false;
 
 gameWindow.addEventListener(
   "click",
   () => {
     console.log(`game started!`);
+    titleScreen.classList.add("started");
   },
   { once: true }
 );
